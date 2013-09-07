015a3d290f451e7d16ee3a923e3f8dcc56a9a173
Jonathan Beezley <jon.beezley@gmail.com>
2013-09-07 23:14:03 +0200

    improving lookup table test

--- testLookupTable.cpp
+++ testLookupTable.cpp
@@ -13,10 +13,11 @@ int main() {
     string dirname("luts");
     int hasError = 0;
    
-    const size_t N = 100;
+    const size_t N = 1000;
     uint8_t A[N];
     uint8_t B[LookupTable::imageSize(N)];
-
+    
+    for(size_t i=0; i<N; i++) A[i] = i % 256;
     dp = opendir(dirname.c_str());
     if (dp == NULL) {
         perror("Cannot open luts directory");
